df3a40906fd4c86a9cb83a5788e9a199bf681116
Mehdi Mehrabani <mmehr1988@gmail.com>
2021-09-17 16:19:04 -0400

    updated seeds data to show more of a realistic article vs. commentary

--- seeds/commentSeeds.js
+++ seeds/commentSeeds.js
@@ -4,42 +4,42 @@ const commentData = [
   {
     user_id: 1,
     post_id: 5,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: "Written by a guy who's spent over a decade in the field of quantum computing.",
   },
   {
     user_id: 4,
     post_id: 4,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: 'The next digital frontier.',
   },
   {
     user_id: 1,
     post_id: 4,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: 'This will be interesting when this takes off.',
   },
   {
     user_id: 3,
     post_id: 5,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: "I know there's probably a subreddit for this, but how would you describe Quantum Computing to a five-year-old?",
   },
   {
     user_id: 3,
     post_id: 2,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: 'In additional to this there was drought in some of the countries where the factories are located prior to the pandemic. This meant they had already been operating at a reduced capacity before the pandemic restrictions hit, further complicating things',
   },
   {
     user_id: 3,
     post_id: 4,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: 'The future is here.',
   },
   {
     user_id: 5,
     post_id: 3,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: 'I still remember when they tried to "embrace and extend" Java with J++',
   },
   {
     user_id: 2,
     post_id: 1,
-    comment_text: 'COMMENT PLACEHOLDER TEXT',
+    comment_text: "Basically like pulling up to the pump & paying the conventional way. I'll take this over wasting memory on my phone with all the different charging apps",
   },
 ];
 
